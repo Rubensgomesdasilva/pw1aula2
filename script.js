1b9f046acb6028326079fce565b47f3c270c3a7c
Rubensgomesdasilva <104952992+Rubensgomesdasilva@users.noreply.github.com>
2025-04-10 19:17:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//     document.addEventListener("DOMContentLoaded", function(){
+
+//      document.getElementById("webBotao").addEventListener("click",function(){
+//         alert("CLICOU KKKK")
+//     })
+    
+    
+// });
+
+// $(document).ready(function(){
+//     $("#webBotao").click(function(){
+//         alert("Clicou mesmo!?")
+//     });
+// });
+    $(document).ready(function(){
+        $("#webBotao").click(function(){
+            alert("clicou mesmo!?kkkk")
+        })
+    })
